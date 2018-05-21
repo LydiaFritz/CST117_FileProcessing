39ce1d1f5763b6898916065bf6d6988d4ab0412d
Lydia Fritz <Lydia.Fritz@gcu.edu>
2018-05-21 12:27:43 -0700

    Add project files.

---
+++ FileProcessingStatistics/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileProcessingStatistics
+{
+    public partial class Form1 : Form
+    {
+        private System.IO.StreamReader sr;
+
+        public Form1( )
+        {
+            InitializeComponent( );
+        }
+
+        private void button1_Click( object sender, EventArgs e )
+        {
+            this.richTextBox1.Text = "";
+            if (openFileDialog1.ShowDialog( ) == System.Windows.Forms.DialogResult.OK)
+            {
+                sr = new System.IO.StreamReader( openFileDialog1.FileName );
+                processFile( );
+                sr.Close( );
+            }
+        }
+
+        private void processFile( )
+        {
+            /*
+             Converts all the words to lower-case
+             Finds the first and last word alphabetically
+             Finds the longest word
+             Finds the word with the most vowels */
+            string firstAlpha = "", lastAlpha = "", longestWord = "";
+            while (!sr.EndOfStream)
+            {
+                
+                string str = sr.ReadLine( );
+                string[] words = str.Split( );
+               
+                foreach (string s in words)
+                {
+                    if (s.Length > 0)
+                    {
+                        ///this.richTextBox1.Text += s.ToLower( ) + "\n";
+                        string sCopy = s.ToLower( );
+                        if (firstAlpha == "")
+                            firstAlpha = sCopy;
+                        if (lastAlpha == "")
+                            lastAlpha = sCopy;
+                        if (longestWord == "")
+                            longestWord = sCopy;
+
+                        if (s.CompareTo( firstAlpha ) < 0)
+                            firstAlpha = sCopy;
+                        if (s.CompareTo( lastAlpha ) > 0)
+                            lastAlpha = sCopy;
+                        if (s.Length > longestWord.Length)
+                            longestWord = sCopy;
+
+
+                    }
+                }
+            }
+                this.richTextBox1.Text += "First alphabetic: " + firstAlpha +
+                    "\nLast alphabetic: " + lastAlpha +
+                    "\nLongest word: " + longestWord + "\n";           
+        }
+
+        private void button2_Click( object sender, EventArgs e )
+        {
+            this.Close( );
+        }
+    }
+}
